b5aa8f7579dec094b92383cae4e0533d5d8ec95d
Graeme Geldenhuys <graemeg@gmail.com>
2018-06-14 23:30:08 +0100

    Re-enable the prevention of stuck modifiers.
    I hit this issue again.

--- src/keymaps/gg_dvorak/config.h
+++ src/keymaps/gg_dvorak/config.h
@@ -3,7 +3,7 @@
 
 #include QMK_KEYBOARD_CONFIG_H
 
-//#define PREVENT_STUCK_MODIFIERS
+#define PREVENT_STUCK_MODIFIERS
 #define PERMISSIVE_HOLD			/* https://github.com/qmk/qmk_firmware/pull/1359/  */
 #define IGNORE_MOD_TAP_INTERRUPT	/* https://github.com/qmk/qmk_firmware/issues/142  */
 
