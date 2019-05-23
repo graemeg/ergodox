153733166006a9474d0cedefc87cab9978be2377
Graeme Geldenhuys <graemeg@gmail.com>
2019-05-23 21:59:53 +0100

    gg_dvorak 1.28.2 changes.
    A duplicate of the 1.28.1 design created with the EZ web configuration
    tool.

--- src/keymaps/gg_dvorak/config.h
+++ src/keymaps/gg_dvorak/config.h
@@ -8,6 +8,9 @@
 #define IGNORE_MOD_TAP_INTERRUPT	/* https://github.com/qmk/qmk_firmware/issues/142  */
 //#define FORCE_NKRO	// Apparently the rules.mk setting doesn't always take effect
 
+#undef DEBOUNCE
+#define DEBOUNCE 15  /* This was the previous default and seems to work better for me. */
+
 #undef MANUFACTURER
 #define MANUFACTURER    Built by Graeme Geldenhuys
 
